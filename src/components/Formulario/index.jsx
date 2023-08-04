d7ea8d61de61b29c19ae267ce577f6775f7a65c6
Tarcio Teles <tarcioapps7@gmail.com>
2023-08-03 23:52:58 -0300

    add

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,12 +1,49 @@
-const Formulario = () => {
-    let materiaA = useState(0);
+import { useState } from "react";
 
-    <form>
-        <input type="number" placeholder="Nota da Matéria A" />
-        <input type="number" placeholder="Nota da Matéria B" />
-        <input type="number" placeholder="Nota da Matéria C" />
-        <p>O aluno foi aprovado!</p>
-    </form>
+const Formulario = () => {
+    let [MateriaA, setMateriaA] = useState(0);
+    let [MateriaB, setMateriaB] = useState(0);
+    let [MateriaC, setMateriaC] = useState(0);
+    let [nome, setNome] = useState('');
+
+    const alteraNome = (evento) => {
+        //console.log(evento.target.value)
+        setNome(estadoAnterior => { 
+            console.log(estadoAnterior);
+            return evento.target.value;
+        })
+    }
+
+    const renderizaResultado = () => {
+        const soma = MateriaA + MateriaB + MateriaC;
+        const media = soma / 3;
+
+        if(media > 6){
+            return(
+                <p>Olá {nome}, foi aprovado!</p>
+            )
+        } else {
+            return (
+                <p>Olá {nome}, foi reprovado</p>
+            )
+        }
+    }
+
+    return (
+        <form>
+            <input type="text" placeholder="Seu nome" onChange={alteraNome} />
+            <input type="number" placeholder="Nota da Matéria A" onChange={evento => setMateriaA(parseInt(evento.target.value))}/>
+            <input type="number" placeholder="Nota da Matéria B" onChange={evento => setMateriaB(parseInt(evento.target.value))}/>
+            <input type="number" placeholder="Nota da Matéria C" onChange={evento => setMateriaC(parseInt(evento.target.value))}/>
+            {renderizaResultado()}
+             {/* 
+            {MateriaA}
+            {MateriaB}
+            {MateriaC}
+            */}
+        </form>
+    )
+
 }
 
 export default Formulario
